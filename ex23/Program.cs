aee0054814d35df5d0d5ab9a6763c24184c560e7
nikita <zherko.na@phystech.edu>
2022-11-21 19:33:26 +0300

    Add exercise 23

---
+++ ex23/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+System.Console.WriteLine("Таблица кубов чисел до N.");
+
+int N = InputN();
+NumberCubes(N);
+
+static int InputN()
+{
+    try
+    {
+        System.Console.Write("Введите N: ");
+        return int.Parse(Console.ReadLine()??"");
+    }
+    catch (Exception exc)
+    {
+        System.Console.WriteLine($"Ошибка ввода! {exc.Message}");
+        System.Console.Write("Введите N (целое число): ");
+        return int.Parse(Console.ReadLine() ??"");
+    }
+    
+}
+
+static void NumberCubes(int N)
+{
+    int index = 1;
+    System.Console.Write($"{N} -> ");
+    while (index <= N)
+    {
+        if(index < N) System.Console.Write($"{Math.Pow(index, 3)}, ");
+        else if(index == N) System.Console.Write($"{Math.Pow(index, 3)}. ");
+        index++;
+    }
+}
+ 
